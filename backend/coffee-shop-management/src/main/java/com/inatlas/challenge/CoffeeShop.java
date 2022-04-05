19881f10f070b3e4f3dc8887f1b2307c1ec0db4f
eur26 <102863916+eur26@users.noreply.github.com>
2022-04-05 22:43:58 +0200

    FIX TOPIC 1
    Change the way an espresso is deducted from the order.

--- backend/coffee-shop-management/src/main/java/com/inatlas/challenge/CoffeeShop.java
+++ backend/coffee-shop-management/src/main/java/com/inatlas/challenge/CoffeeShop.java
@@ -14,13 +14,30 @@ public class CoffeeShop {
         System.out.println("======================================");
         boolean hasMoreThanOneLatte = this.orders.stream().anyMatch(p -> p.getName().equals("Latte") && p.getQtt() > 1);
         long countLatte = this.orders.stream().filter(p -> p.getName().equals("Latte")).count();
+        
+        Product esp1;
+        boolean deleteQttEspresso = false;
+        
+        //para que contabilice bien el total
+        orders = organizeOrder(this.orders);
+        
         if (hasMoreThanOneLatte || countLatte > 1) {
-            Product exp1 = this.orders.stream().filter(p -> p.getName().equals("Espresso")).findFirst().get();
-            exp1.setDiscount(true);
+            esp1 = this.orders.stream().filter(p -> p.getName().equals("Espresso")).findFirst().get();
+            if(esp1.getQtt().equals(1)) {
+            	esp1.setDiscount(true);
+            } else {
+            	deleteQttEspresso=true;
+            }
         }
+        
+        
+        final boolean deleteQtt = deleteQttEspresso;
+        
         Double total = this.orders.stream().map(p -> {
-            System.out.println(p);
-            return Double.valueOf(p.getPrice().split("\\$")[1])*p.getQtt();
+        	System.out.println(p.getQtt()+" "+p);
+        	return p.getName().equals("Espresso") && deleteQtt? 
+        			Double.valueOf(p.getPrice().split("\\$")[1])*(p.getQtt()-1)
+        			:Double.valueOf(p.getPrice().split("\\$")[1])*p.getQtt();
         }).reduce(0.0, (a, b) -> a + b);
         System.out.println("----------------");
         System.out.println("Total: $" + total);
@@ -30,4 +47,17 @@ public class CoffeeShop {
     public void printMenu() {
         // Print whole menu
     }
+    
+    private List<Product> organizeOrder(List<Product> orders2) {
+    	List<Product> ordersTmp = new ArrayList<>();
+		for(Product p : orders2) {
+			if(ordersTmp.stream().anyMatch(o -> o.getName().equals(p.getName()))) {
+				Product prodTmp = ordersTmp.stream().filter(ot -> ot.getName().equals(p.getName())).findFirst().get();
+				prodTmp.setQtt(p.getQtt()+prodTmp.getQtt());
+			}else {
+				ordersTmp.add(p);
+			}
+		}
+		return ordersTmp;
+	}
 }
