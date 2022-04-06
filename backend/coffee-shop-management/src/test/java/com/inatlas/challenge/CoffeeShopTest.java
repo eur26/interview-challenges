3c046de12306da59981792797ab35783e133004c
eur26 <102863916+eur26@users.noreply.github.com>
2022-04-06 22:32:40 +0200

    Adapt existing tests
    Change and adapt tests

--- backend/coffee-shop-management/src/test/java/com/inatlas/challenge/CoffeeShopTest.java
+++ backend/coffee-shop-management/src/test/java/com/inatlas/challenge/CoffeeShopTest.java
@@ -1,19 +1,31 @@
 package com.inatlas.challenge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class CoffeeShopTest {
     
     @Test
     public void testTakeMyFirstOrder(){
+    	System.out.println("TEST 1");
         CoffeeShop coffeeShop = new CoffeeShop();
-        System.out.println("TEST 1");
         coffeeShop.printMenu();
-        coffeeShop.takeOrder("Latte", 1);
-        coffeeShop.takeOrder("Espresso", 1);
-        coffeeShop.takeOrder("Sandwich", 1);
-        coffeeShop.printReceipt();
+        Client cliente = new Client("James");
+		List<Product> order = new ArrayList<>();
+		coffeeShop.takeOrder(cliente, order, "Latte", 1);
+		coffeeShop.takeOrder(cliente, order, "Espresso", 1);
+		coffeeShop.takeOrder(cliente, order, "Sandwich", 1);
+		try {
+			cliente.setOrder(order);
+		} catch (Exception e) {
+			System.err.println("Error" + e);
+		}
+		System.out.println(cliente);
+		coffeeShop.printReceipt(order);
         // Total should be $18.1
+		
     }
 
     @Test
@@ -21,10 +33,12 @@ public class CoffeeShopTest {
         System.out.println("TEST 2");
         CoffeeShop coffeeShop = new CoffeeShop();
         coffeeShop.printMenu();
-        coffeeShop.takeOrder("Latte", 2);
-        coffeeShop.takeOrder("Espresso", 1);
-        coffeeShop.takeOrder("Sandwich", 1);
-        coffeeShop.printReceipt();
+        Client cliente = new Client("Laura");
+        List<Product> order = new ArrayList<>();
+        coffeeShop.takeOrder(cliente, order, "Latte", 2);
+        coffeeShop.takeOrder(cliente, order, "Espresso", 1);
+        coffeeShop.takeOrder(cliente, order, "Sandwich", 1);
+        coffeeShop.printReceipt(order);
         // Total should be $15.1
     }
 
@@ -33,11 +47,13 @@ public class CoffeeShopTest {
         System.out.println("TEST 3");
         CoffeeShop coffeeShop = new CoffeeShop();
         coffeeShop.printMenu();
-        coffeeShop.takeOrder("Latte", 1);
-        coffeeShop.takeOrder("Latte", 1);
-        coffeeShop.takeOrder("Espresso", 1);
-        coffeeShop.takeOrder("Sandwich", 1);
-        coffeeShop.printReceipt();
+        Client cliente = new Client("Laura");
+        List<Product> order = new ArrayList<>();
+        CoffeeShop.takeOrder(cliente, order, "Latte", 1);
+        CoffeeShop.takeOrder(cliente, order, "Latte", 1);
+        coffeeShop.takeOrder(cliente, order, "Espresso", 1);
+        coffeeShop.takeOrder(cliente, order, "Sandwich", 1);
+        coffeeShop.printReceipt(order);
         // Total should be $15.1
     }
 
@@ -46,11 +62,13 @@ public class CoffeeShopTest {
         System.out.println("TEST 4");
         CoffeeShop coffeeShop = new CoffeeShop();
         coffeeShop.printMenu();
-        coffeeShop.takeOrder("Latte", 2);
-        coffeeShop.takeOrder("Espresso", 1);
-        coffeeShop.takeOrder("Espresso", 1);
-        coffeeShop.takeOrder("Sandwich", 1);
-        coffeeShop.printReceipt();
+        Client cliente = new Client("Laura");
+        List<Product> order = new ArrayList<>();
+        coffeeShop.takeOrder(cliente, order, "Latte", 2);
+        coffeeShop.takeOrder(cliente, order, "Espresso", 1);
+        coffeeShop.takeOrder(cliente, order, "Espresso", 1);
+        coffeeShop.takeOrder(cliente, order, "Sandwich", 1);
+        coffeeShop.printReceipt(order);
         // Total should be $18.1
     }
     
@@ -59,11 +77,13 @@ public class CoffeeShopTest {
         System.out.println("TEST 5");
         CoffeeShop coffeeShop = new CoffeeShop();
         coffeeShop.printMenu();
-        coffeeShop.takeOrder("Latte", 3);
-        coffeeShop.takeOrder("Espresso", 3);
-        coffeeShop.takeOrder("Espresso", 2);
-        coffeeShop.takeOrder("Sandwich", 5);
-        coffeeShop.printReceipt();
+        Client cliente = new Client("Laura");
+        List<Product> order = new ArrayList<>();
+        coffeeShop.takeOrder(cliente, order, "Latte", 3);
+        coffeeShop.takeOrder(cliente, order, "Espresso", 3);
+        coffeeShop.takeOrder(cliente, order, "Espresso", 2);
+        coffeeShop.takeOrder(cliente, order, "Sandwich", 5);
+        coffeeShop.printReceipt(order);
         // Total should be $18.1
     }
 }
